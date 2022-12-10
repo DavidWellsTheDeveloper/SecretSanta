245811373a55a932ff7bae60a29b613e22aa346f
dave <dave1.t.wells@gmail.com>
2022-12-10 15:46:00 -0700

    Add secret santa models

--- backend/secretSanta/models.py
+++ backend/secretSanta/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    eventName = models.CharField(max_length=400)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    pairingDate = models.DateTimeField(null=True)
+    modifiedDate = models.DateTimeField(auto_now=True)

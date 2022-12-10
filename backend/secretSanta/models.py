d2fe804ea6e6cefe69f29b9f637a0553edd5cea7
Rick Wells <Ricwells4002@gmail.com>
2022-12-10 15:59:32 -0700

    add EventUser model

--- backend/secretSanta/models.py
+++ backend/secretSanta/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,3 +9,9 @@ class Event(models.Model):
     createdDate = models.DateTimeField(auto_now_add=True)
     pairingDate = models.DateTimeField(null=True)
     modifiedDate = models.DateTimeField(auto_now=True)
+
+class EventUser(models.Model):
+    id = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    eventId = models.ForeignKey(Event, on_delete=models.CASCADE)
+    isOwner = models.BooleanField(default=False)

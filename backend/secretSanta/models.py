e42ecdc2faaeb900f3719cae82ce68ed3846e30a
Rick Wells <Ricwells4002@gmail.com>
2022-12-10 16:18:13 -0700

    textField

--- backend/secretSanta/models.py
+++ backend/secretSanta/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
-    eventName = models.CharField(max_length=400)
+    eventName = models.TextField()
     createdDate = models.DateTimeField(auto_now_add=True)
     pairingDate = models.DateTimeField(null=True)
     modifiedDate = models.DateTimeField(auto_now=True)

e1b7546df72e2ecbcede9cacf2aa874638e9be02
dave <dave1.t.wells@gmail.com>
2022-12-11 15:19:09 -0700

    Include auth endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,7 @@ from rest_framework import routers
 
 
 urlpatterns = [
-    path('secretSanta/', include('secretSanta.urls')),
+    path('', include('secretSanta.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
